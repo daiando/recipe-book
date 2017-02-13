e8fc56af22085c39d94cf01a19b26b9d64b552b6
Ando <dai.ando@p42201.intra.rakuten.co.jp>
2017-02-13 17:33:21 +0900

    practice flexbox

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -5,12 +5,17 @@ import { RecipeItemComponent } from './recipe-item.component';
 
 @Component({
   selector: 'rb-recipe-list',
-  templateUrl: './recipe-list.component.html'
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy', 'Dummy', 'http://24.media.tumblr.com/tumblr_lkfk9fZfr91qbpotpo1_400.jpg');
+  recipe = new Recipe(
+    'Ruby on Rails',
+    'One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin',
+    '../../../assets/images/1486453061_ruby-rails.png'
+  );
 
   constructor() { }
 

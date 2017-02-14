80fb59c61e631049dcfdf539973c6ad9f40c9c7e
DAI ANDO <daifloss@gmail.com>
2017-02-15 01:52:58 +0900

    finish login

--- src/assets/custom-modal/test-animate.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('#demo01').animatedModal({
-    modalTarget: 'content01',
-    animatedIn: 'bounceInUp', //OPENアニメーション
-    animatedOut: 'bounceOutUp', //CLOSEアニメーション
-    animationDuration: '1s', //アニメーションにかける秒数
-    color: 'rgba(0, 0, 0, 0.44)', //背景色
-  });
-});

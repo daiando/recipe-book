aaf0885fd06c831ca76a9fc95f5c5ea5771537d6
“Dai <dai.ando@rakuten.com>
2017-03-01 15:35:37 +0900

    lazy routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,9 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from './header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { GreetingComponent } from './greeting/greeting.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component'
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeService } from './recipes/recipe.service'
-
 @Component({
   selector: 'rb-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [RecipeService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
+import { HomeComponent } from "./home.component";
 import { GreetingComponent } from './greeting/greeting.component';
 import { DropdownDirective } from './dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeService } from './recipes/recipe.service';
 
 import { routing } from './app.routing';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start.component';
+import { ShoppingListModule } from "./shopping-list/shopping-list.module";
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    ShoppingListComponent,
-    ShoppingListAddComponent,
+    HomeComponent,
     GreetingComponent,
-    DropdownDirective,
-    RecipeEditComponent,
-    RecipeStartComponent
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
     ReactiveFormsModule,
+    ShoppingListModule,
     routing
   ],
-  providers: [ShoppingListService],
+  providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
